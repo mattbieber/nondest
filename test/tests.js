05aef920ba2c85bce9908e5165fee68834b74e96
mattbieber <matt@spaceboy.co>
2014-01-01 10:58:53 -0800

    updating tests

--- test/tests.js
+++ test/tests.js
@@ -1,56 +1,59 @@
 /**
  * nondest mocha tests
  */
+describe('nondest tests', function() {
 
-var path = require('path'),
-	fs = require('fs'),
-	should = require('should');
+	var path = require('path'),
+		fs = require('fs'),
+		should = require('should');
 
-var nondest,
-	fixture,
-	err;
-
-describe('nondest test', function() {
-
-	before(function(done) {
-		nondest = require('../lib/nondest');
-		fixture = path.resolve('test/fixtures/test01');
-		done();''
-	});
-
-	after(function(done) {
-		done();
-	});
-
-
-	it('should be a nondest object', function(done) {
-		nondest.should.be.an.Object;
-		nondest.should.have.ownProperty('create', 'remove');
-
-		done();
-	});
-
-	it('should have created a directory', function(done) {
-		this.timeout(0);
-		nondest.create(fixture, {}).on('available', function(tmppath) {
-			
-			var _isDirectory = fs.statSync(tmppath).isDirectory();
-			_isDirectory.should.be.true;
-
-			console.log(err);
-			done();
+	describe('success tests', function() {
 		
-		}).on('error', function(e) {
-			err = e;
+		var nondest_success,
+		fixture_success,
+		err_success;
 
+		before(function(done) {
+			nondest_success = require('../lib/nondest');
+			fixture_success = path.resolve('test/fixtures/test01');
 			done();
 		});
 
-	});
+		after(function(done) {
+			nondest_success = null;
+			done();
+		});
 
-	it('should have not have thrown error while creating temp copy', function(done) {
-		err.should.equel(undefined);
-		done();
-	});
+		it('should be a nondest object', function(done) {
+			nondest_success.should.be.an.Object;
+			nondest_success.should.have.ownProperty('create', 'remove');
+			done();
+		});
 
+		it('should have created a directory', function(done) {
+			this.timeout(0);
+			nondest_success.create(fixture_success, {}).on('available', function(tmppath) {		
+				var _isDirectory = fs.statSync(tmppath).isDirectory();
+				_isDirectory.should.be.true;
+				done();
+			
+			}).on('error', function(e) {
+				err_success = e;
+				done();
+			});
+		});
+
+		it('should have not have thrown error while creating temp copy', function(done) {
+			should.strictEqual(undefined, err_success);
+			done();	
+		});
+
+		it('should remove temp dir upon request', function(done) {
+			this.timeout(0);	
+			nondest_success.remove(function(result){
+				should.strictEqual(undefined, result);
+				done();	
+			});
+		});
+	});
 });
